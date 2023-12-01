51b16446db1dc73895a9680ea00bb66b5ec07955
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-01 20:49:08 +0000

    var+data

---
+++ 01/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 545638
+let accountEmail =  "sdhfgt@gmail.com"
+var accountPassword = "5346"
+accountCity = "indore"
+
+accountEmail = "sdfjh@gmail.com"
+accountPassword = "254735"
+accountCity  ="aligarh"
+
+console.log(accountId);
+console.table([accountId , accountEmail , accountPassword , accountCity])
