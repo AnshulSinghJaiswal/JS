e45e9e129371119f38612660096037f8c43f65ec
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-05 16:11:16 +0000

    conversion of datatypes

---
+++ 01/03_datatype_and_converssion.js
@@ -0,0 +1,18 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+
+console.log(valueInNumber);
+
+
+//numbert to string conversion
+let age = 3452
+console.log(typeof age);
+
+let numbertostring =String(age)
+ console.log(typeof numbertostring);
