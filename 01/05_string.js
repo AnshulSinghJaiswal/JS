ebde8820ce9625207dd68b9681eb5d6abc1628d8
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 08:27:49 +0000

    string

---
+++ 01/05_string.js
@@ -0,0 +1,9 @@
+const name = "anshul"
+const age = 21
+
+//console.log(name + age + "sdgofhu");
+
+console.log(`my name is ${name} and my age is ${age}`);
+
+const gameName = new String ('AnshulSj')
+console.log(gameName[0]);
