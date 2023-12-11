39c73a4b53608965e50b0483ddc812258e45cbb4
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 16:37:37 +0000

    arrow

---
+++ 03/arrow.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "anshul",
+    price: 125,
+    welcomemessage : function(){
+        console.log(`${this.username} , welcome back home`);
+    }
+}
+user.welcomemessage()
+
+
+// user.username = "ANSHUL"
+// user.welcomemessage()
+
