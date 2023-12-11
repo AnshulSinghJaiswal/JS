46fe4837cdd84843f7c0d634889f162e5df7b5ff
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 15:48:17 +0000

    fuction

---
+++ 03/fuct1.js
@@ -0,0 +1,14 @@
+function add(number1 , number2){
+    //let result = number1+number2 
+    //return result
+    return number1+number2
+}
+
+const result = add(5,9)
+//console.log('result : ',result);
+
+function logoinuser(username){
+    return `${username} just logged in`
+}
+//logoinuser('Anshul')-----ye print nhi hoga because humne kuch bola hi nhi ki print karo ya store karo
+console.log(logoinuser('anshul')); //now it will be printed
