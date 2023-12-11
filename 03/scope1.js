5e64c78ca12d3cbe12e7d53bf3674156e8fddbff
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 16:13:32 +0000

    scope

---
+++ 03/scope1.js
@@ -0,0 +1,21 @@
+let a = 300
+if(true){
+    let a = 10
+    const b  = 20
+    //console.log('inner :', a);
+}
+
+//console.log(a);
+
+function one(){
+    const username = "Anshul"
+
+    function two(){
+        const website  ='youtube'
+        console.log(username);
+    }
+    //console.log(website);
+    
+    two()
+}
+one()
