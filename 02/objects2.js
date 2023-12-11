5e7ec01fc9a6e061bd5de31c4db5a3e2721a1ba2
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 14:17:19 +0000

    jason

---
+++ 02/objects2.js
@@ -0,0 +1,14 @@
+//combining
+const obj1 = {1: 'a' , 2: 'sda',}
+const obj2 = {3: 'afg' , 4: 'sgda',}
+const obj3 = Object.assign({}, obj1, obj2)
+  
+// console.log(obj3);
+
+const cousre = {
+    cousrename : 'MA201',
+    price : "500000",
+    instructor : "Ananad Parey"
+}
+const{instructor} = cousre
+console.log(instructor);
