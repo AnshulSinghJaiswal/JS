7c2cbe0a221c67b2ed56d44451d8fe14cd1c06db
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 10:17:18 +0000

    array 1

---
+++ 02/arrays.js
@@ -0,0 +1,25 @@
+//array
+const myarr = [0,1,2,3,4,5]
+const myarr2 = new Array(1,2,3,4)
+
+console.log(myarr[1])
+
+//array methods
+// myarr.push(6)
+// myarr.push(7)
+// myarr.pop()
+
+// myarr.unshift(9)
+// myarr.shift()
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(8));
+
+// const newarr = myarr.join()
+// console.log(myarr);
+// console.log(newarr);
+// console.log(typeof newarr);
+
+const myn = myarr.splice(1,3)
+console.log(myarr);
+console.log(myn);
