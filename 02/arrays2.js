6d1034a7ca1347bd5d2dc386d7e0142fd1943c3f
AnshulSinghJaiswal <141642080+AnshulSinghJaiswal@users.noreply.github.com>
2023-12-11 10:17:35 +0000

    array 2

---
+++ 02/arrays2.js
@@ -0,0 +1,12 @@
+const one = ["dsf", "sdfwers", "dsasf", "shgjkdfs","sdf"]
+const two = ["tsb", "oancv", "asdool", "aosnbfvc"]
+
+// one.push(two)
+// console.log(one);
+// console.log(one[5][2]); 
+
+// const three = one.concat(two)
+// console.log(three);
+
+ const four = [...one,...two]
+ console.log(four);
